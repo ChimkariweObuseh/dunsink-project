3bfb3bfbec63a6cfca16c0afcf45371b02bca399
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-24 20:54:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const blocks = document.getElementsByClassName('blocks');
 
 for (let i = 1; i < 400; i++) {
   banner.innerHTML += "<div class='blocks'></div>";
+  blocks[i].style.animationDelay = `${i * 0.05}s`;
 }
