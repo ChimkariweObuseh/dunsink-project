6ffc9e7082768e436a0b0c912a8bc61a799809eb
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-25 20:18:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ window.onmousedown = e => {
   track.dataset.mouseDownAt = e.clientX;
 }
 
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.prevPercentage;
+}
+
 window.onmousemove = e => {
   if (track.dataset.mouseDownAt === "0") return;
   
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+  Math.min(nextPercentage, 0);
+  Math.max(nextPercentage, -100);
 
-  const percentage = (mouseDelta / maxDelta) * -100;
-
-  track.style.transform = `translate(${percentage}%, -50%)`;
+  track.dataset.percentage = nextPercentage;
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
 }
