1722de0ddccefc38e02bdbd5768386b881e836ff
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-25 19:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const track = document.getElementById('image-track');
+
 window.onmousedown = e => {
-  e.clientX;
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmousemove = e => {
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * 100;
+
+  track.style.transform = `translate(${percentage}%, -50%)`;
 }
